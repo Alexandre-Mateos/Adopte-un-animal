92111b18b8911d1fe0b91ab9f0473f02d285979e
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-04 09:52:33 +0200

    ajout du script pour stocker puis afficher l'animal adopté

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,2 +1,20 @@
+let form = document.querySelector("#formulaire");
 let animalName = document.querySelector("#nom-animal");
 let animalRace = document.querySelector("#race");
+let adoptedAnimal = document.querySelector("#mon-animal");
+
+let myAnimalName = localStorage.getItem("nom_animal");
+let myAnimalRace = localStorage.getItem("race_animal");
+
+let paraAnimalName = document.createElement("p");
+paraAnimalName.innerHTML = myAnimalName;
+adoptedAnimal.insertAdjacentElement("beforeend", paraAnimalName);
+
+let paraAnimalRace = document.createElement("p")
+paraAnimalRace.innerHTML = myAnimalRace;
+adoptedAnimal.insertAdjacentElement("beforeend", paraAnimalRace);
+
+form.addEventListener("submit", () => {
+    localStorage.setItem("nom_animal", animalName.value);
+    localStorage.setItem("race_animal", animalRace.value);
+})
