ed408e54e853e71dd7a23d5c88b9f04af45421ef
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-04 09:55:55 +0200

    creation de la fonction adopter animal

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,18 +3,22 @@ let animalName = document.querySelector("#nom-animal");
 let animalRace = document.querySelector("#race");
 let adoptedAnimal = document.querySelector("#mon-animal");
 
-let myAnimalName = localStorage.getItem("nom_animal");
-let myAnimalRace = localStorage.getItem("race_animal");
-
-let paraAnimalName = document.createElement("p");
-paraAnimalName.innerHTML = myAnimalName;
-adoptedAnimal.insertAdjacentElement("beforeend", paraAnimalName);
-
-let paraAnimalRace = document.createElement("p")
-paraAnimalRace.innerHTML = myAnimalRace;
-adoptedAnimal.insertAdjacentElement("beforeend", paraAnimalRace);
+adopterAnimal();
 
 form.addEventListener("submit", () => {
-    localStorage.setItem("nom_animal", animalName.value);
-    localStorage.setItem("race_animal", animalRace.value);
-})
+  localStorage.setItem("nom_animal", animalName.value);
+  localStorage.setItem("race_animal", animalRace.value);
+});
+
+function adopterAnimal() {
+  let myAnimalName = localStorage.getItem("nom_animal");
+  let myAnimalRace = localStorage.getItem("race_animal");
+
+  let paraAnimalName = document.createElement("p");
+  paraAnimalName.innerHTML = myAnimalName;
+  adoptedAnimal.insertAdjacentElement("beforeend", paraAnimalName);
+
+  let paraAnimalRace = document.createElement("p");
+  paraAnimalRace.innerHTML = myAnimalRace;
+  adoptedAnimal.insertAdjacentElement("beforeend", paraAnimalRace);
+}
